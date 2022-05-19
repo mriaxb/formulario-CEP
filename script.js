c8a45c356b07a4c8c30103876c413c56b6a20210
mriaxb <88912846+mriaxb@users.noreply.github.com>
2022-05-19 14:00:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const cep = document.querySelector("#cep")
+var validacep = /^[0-9]{8}$/
+const showData = (result) => {
+    
+    for(const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo]
+        }
+    }
+
+}
+
+cep.addEventListener("blur", (e) =>{
+    let search = cep.value.replace("-", "")
+    const cepValue = cep.value
+    var validacep = /^[0-9]{8}$/;
+    addLoader()
+    if(validacep.test(cepValue)){
+        const options = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default'
+        }
+        addLoader()
+        fetch(`https://viacep.com.br/ws/${search}/json`, options)
+        .then(response =>{response.json()
+            .then(data => showData(data))
+            successValidation(cep)
+            addLoader()
+        })
+        .catch(errorValidation(cep, 'CEP nao encontrado'))
+        
+    }
+    else{
+        apagaForm();
+        errorValidation(cep, 'CEP inválido')
+    }
+    
+
+})
+// reset
+function apagaForm() {
+	document.getElementById('form').reset();
+}
+
+
+//função de erro
+function errorValidation(input, message){
+    const formgroup = input.parentElement;
+    const small = formgroup.querySelector('small')
+
+    small.innerText = message
+
+    formgroup.className = 'form-group error'
+}
+
+//funçao de sucesso
+function successValidation(input){
+    const formgroup = input.parentElement;
+    
+    formgroup.className = 'form-group success'
+}
+
+function addLoader(){
+    const ruaLoad = document.getElementById('ruaLoad')
+    ruaLoad.className = "loader-visib"
+}
